cb7305258c528ddd58e1bbbd1838718d35452c6d
Jon <jon@stencyl.com>
2012-09-10 18:19:51 +0000

    Report file/line num in console

--- com/nmefermmmtools/debug/Console.hx
+++ com/nmefermmmtools/debug/Console.hx
@@ -241,7 +241,7 @@ class Console
 	
 	private static function traceHandler(value:Dynamic, ?positionInformation:PosInfos):Void 
 	{
-		untyped __global__["trace"]("Stencyl:" + value);
+		untyped __global__["trace"]("Stencyl:" + positionInformation.className+"."+positionInformation.methodName+"("+positionInformation.lineNumber+"): " + value);
 		
 		// If the passed value is null, call the writeNull method.
 		if (value == null) {
